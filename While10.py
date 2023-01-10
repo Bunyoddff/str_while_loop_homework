7c664fbb21ee08afed8c6efcf1698755f522b30b
Bunyoddff <bkhusanov444@gmail.com>
2023-01-10 11:26:39 +0500

    py10

--- While10.py
+++ While10.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+
+    i=0
+    sum=0
+    while i<=len(s)-1:
+        if int(s[i])%2!=0:
+            sum+=int(s[i])
+        i+=1
+    return sum
+print(main('234567892'))
