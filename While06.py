91963478a5cbe2095e03d75d5d0226b2b5c00aa6
Bunyoddff <bkhusanov444@gmail.com>
2023-01-10 11:45:24 +0500

    p6

--- While06.py
+++ While06.py
@@ -10,6 +10,7 @@ def main(s):
     d=0
     i=0
     while i<=len(s)-1:
+        
         if not (s[i] == 'a'or s[i]== 'e'or s[i]== 'i'or s[i]== 'o'or s[i]== 'u' or s[i]== 'A'or s[i]== 'E'or s[i]== 'I'or s[i]== 'O'or s[i]== 'U' ):
             d+=1
         i+=1
