fe394b3a1ccea95836b36d0baf68a98ea5ccab92
Bunyoddff <bkhusanov444@gmail.com>
2023-01-10 11:39:31 +0500

    py09

--- While09.py
+++ While09.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    sum=0
+    while i<=len(s)-1:
+        sum+=int(s[i])
+        i+=1
+
+    return sum
+print(main('234'))
